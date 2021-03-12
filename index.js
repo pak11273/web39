b1980006a5d7fc49b315596c1f5f0aa84f6a3a3d
pak11273 <pak11273@gmail.com>
2021-03-12 15:07:48 -0600

    adds cors

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hi");
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const server = express();
+server.use(express.json());
+
+if (process.NODE_ENV === "development") {
+  // on heroku env is NODE_ENV => production
+  const cors = require("cors");
+  server.use(cors());
+}
